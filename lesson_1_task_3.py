6acb2b8f087f61b0456c40b4b3b5bb4dceef27fb
Essences12 <essencesimsay@gmail.com>
2026-09-18 13:04:04 -0300

    Hello

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,4 +1,4 @@
-first_name = input("Рамазан: ")
-last_name = input("Тамбиев: ")
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
 
 print(f"Вас зовут: {last_name} {first_name}")
