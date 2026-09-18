e8d4b98c65ad3cf8c7cadfbf8fad945c3fdfefd9
Essences12 <essencesimsay@gmail.com>
2026-09-18 13:05:23 -0300

    1

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,4 +1,4 @@
-first_name = input("Введите ваше имя: ")
-last_name = input("Введите вашу фамилию: ")
+first_name = input("Введите имя: ")
+last_name = input("Введите фамилию: ")
 
 print(f"Вас зовут: {last_name} {first_name}")
